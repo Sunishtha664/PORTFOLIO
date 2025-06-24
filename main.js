b1836333c78d9afc0cf172df9020a9265ccab3a8
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-06-24 23:15:09 +0545

    added animations

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".text", {
+    strings:["WordPress Developer", "Web Developer", "SEO Practitioner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true, 
+});
